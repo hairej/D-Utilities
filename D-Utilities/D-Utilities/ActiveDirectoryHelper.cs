362bdd59d1ce026ab7d6043e11f3c3db1ea09211
Dysean <dysean1@gmail.com>
2017-08-17 14:42:30 -0400

    Added Active Directory Helper.

---
+++ D-Utilities/D-Utilities/ActiveDirectoryHelper.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.DirectoryServices.AccountManagement;
+using System.DirectoryServices;
+
+namespace D_Utilities
+{
+    public class ActiveDirectoryHelper
+    {
+        /// <summary>
+        /// Validaites user credentials against Active Directory.
+        /// </summary>
+        /// <param name="username"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        public static bool ValidateUser(string username, string password)
+        {
+            bool validated = false;
+
+            PrincipalContext context = new PrincipalContext(ContextType.Domain, "domain.com");
+
+            if (context.ValidateCredentials(username, password))
+            {
+                validated = true;
+            }
+
+            return validated;
+        }
+
+        /// <summary>
+        /// Verifies that user is a part of the specified group.
+        /// </summary>
+        /// <param name="username"></param>
+        /// <param name="groupname"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public static bool IsUserInGroup(string username, string groupname)
+        {
+            PrincipalContext mPrincipalContext = new PrincipalContext(ContextType.Domain, "domain.com");
+
+            var foundUserInGroup = false;
+
+            GroupPrincipal mGroupPrincipal = null;
+
+            try
+            {
+                mGroupPrincipal = GroupPrincipal.FindByIdentity(mPrincipalContext, groupname);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            if (mGroupPrincipal != null)
+            {
+                foreach (var member in mGroupPrincipal.GetMembers(true))
+                {
+                    try
+                    {
+                        if (member.SamAccountName.Equals(username))
+                        {
+                            foundUserInGroup = true;
+
+                            break;
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        throw ex;
+                    }
+                }
+            }
+
+            if (mGroupPrincipal != null)
+            {
+                mGroupPrincipal.Dispose();
+            }
+
+            if (mPrincipalContext != null)
+            {
+                mPrincipalContext.Dispose();
+            }
+
+            return foundUserInGroup;
+        }
+
+        /// <summary>
+        /// Gets the User Principal for the specified user.
+        /// </summary>
+        /// <param name="UserName"></param>
+        /// <returns></returns>
+        public static UserPrincipal GetUserInformaiton(string UserName)
+        {
+            PrincipalContext ctx = new PrincipalContext(ContextType.Domain, "domain.com");
+
+            UserPrincipal mUserPrincipal = new UserPrincipal(ctx);
+
+            mUserPrincipal = UserPrincipal.FindByIdentity(ctx, UserName);
+
+            return mUserPrincipal;
+        }
+
+        /// <summary>
+        /// Gets the entire Directory Entry for the specified user.
+        /// </summary>
+        /// <param name="UserName"></param>
+        /// <returns></returns>
+        public DirectoryEntry GetFullUserDirectoryEntry(string UserName)
+        {
+            UserPrincipal mUserPrincipal = GetUserInformaiton(UserName);
+
+            try
+            {
+                if (mUserPrincipal.GetUnderlyingObjectType() == typeof(DirectoryEntry))
+                {
+                    return (DirectoryEntry)mUserPrincipal.GetUnderlyingObject();
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch
+            {
+                return null;
+            }
+       
+        }
+    }
+}
